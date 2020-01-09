eb6170db7e231a1214bf7075ab9d99457217ef2e
Gieraldo21 <liviotrigieraldo06@gmail.com>
2020-01-09 16:21:49 +0700

    tambah id pw
    aldo.com
    aldo

--- PembelianObat/PembelianObat/FMenu.Designer.vb
+++ PembelianObat/PembelianObat/FMenu.Designer.vb
@@ -87,7 +87,7 @@ Partial Class FMenu
         'PembelianToolStripMenuItem
         '
         Me.PembelianToolStripMenuItem.Name = "PembelianToolStripMenuItem"
-        Me.PembelianToolStripMenuItem.Size = New System.Drawing.Size(140, 22)
+        Me.PembelianToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
         Me.PembelianToolStripMenuItem.Text = "Pemesanan"
         '
         'TransaksiToolStripMenuItem
@@ -100,7 +100,7 @@ Partial Class FMenu
         'PembayaranToolStripMenuItem
         '
         Me.PembayaranToolStripMenuItem.Name = "PembayaranToolStripMenuItem"
-        Me.PembayaranToolStripMenuItem.Size = New System.Drawing.Size(140, 22)
+        Me.PembayaranToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
         Me.PembayaranToolStripMenuItem.Text = "Pembayaran"
         '
         'BarangToolStripMenuItem
@@ -140,6 +140,7 @@ Partial Class FMenu
         'Label1
         '
         Me.Label1.AutoSize = True
+        Me.Label1.Cursor = System.Windows.Forms.Cursors.HSplit
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.75!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label1.ForeColor = System.Drawing.Color.Aqua
         Me.Label1.Location = New System.Drawing.Point(94, 202)
